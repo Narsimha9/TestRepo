1d5f49a61054daaf5ecae8bc81acb8b8aea5019f
B Narsimhulu <narsimhulu.464@gmail.com>
2020-05-10 18:55:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,27 +2,19 @@
 
 const express=require('express');
 const app=express();
-var express_logger = require('./express-logger-unique-req-id');
 
-const morgan=require('morgan')
-var winston=require('./winston')
 
-app.use(morgan('combined',{stream:winston.stream}))
-
-express_logger.initializeLogger(app);;
-let logger = express_logger.getLogger()
 
 //logger.debug('First message');
 
 app.get('/test',(req,res)=>
 {
 res.send("testing done");
-logger.info('get')
 })
 app.post('/work',(req,res) =>
 {    
     res.send("ok")
-    logger.info('post')
+   
 })
 app.get('/',(req,res) =>{
 console.log("hello")
